c2a9e09bf369536cc9fccade93a07d53cbb4e702
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-03-03 16:33:51 +0100

    component state and whatnot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment, Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>hello</h1>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
