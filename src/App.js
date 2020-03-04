af604cee50a15d4327bbed87e2e2defa8eb08f5a
benjamienSD <benjamien.deprez.dtp@gmail.com>
2020-03-04 10:24:38 +0100

    add UserItem and Users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Fragment, Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
-import "./App.css";
+import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Navbar />
-        <UserItem />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Navbar />
+				<Users />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class UserItem extends Component {
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://api.github.com/users/mojombo"
-  };
-
-  render() {
-    const { avatar_url, login, html_url } = this.state;
-    return (
-      <div>
-        <div className='car text-center'>
-          <img
-            src={avatar_url}
-            alt='avatar'
-            className='round-img'
-            style={{ width: "60px" }}
-          />
-          <h3>{login}</h3>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            more
-          </a>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { avatar_url, login, html_url } = this.props.user;
+		return (
+			<div>
+				<div className='card text-center'>
+					<img
+						src={avatar_url}
+						alt='avatar'
+						className='round-img'
+						style={{ width: '60px' }}
+					/>
+					<h3>{login}</h3>
+					<a href={html_url} className='btn btn-dark btn-sm my-1'>
+						more
+					</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default UserItem;
